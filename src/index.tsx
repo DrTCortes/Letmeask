064464b10f4f0515acb74f72b785691f8bafc1e5
DrTCortes <thiagocortes@outlook.com.br>
2021-07-04 22:46:29 -0300

    Criando página Home

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Button} from './components/Button'
 
 import './services/firebase'
+
+import './styles/global.scss'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Button text={'ContLeft: '}/>
-    <Button text={'ContRight: '}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
