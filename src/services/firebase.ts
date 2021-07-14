862d867f2a6f3359e4ccfe0299e3e468bdf3d3a1
DrTCortes <thiagocortes@outlook.com.br>
2021-07-13 23:46:17 -0300

    Implementando criação de salas no Firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC53_IxSSmahV9a-uv5MtsduWhz_XHK2QE",
-  authDomain: "letmeask-831b8.firebaseapp.com",
-  databaseURL: "https://letmeask-831b8-default-rtdb.firebaseio.com",
-  projectId: "letmeask-831b8",
-  storageBucket: "letmeask-831b8.appspot.com",
-  messagingSenderId: "34077705359",
-  appId: "1:34077705359:web:bf82c7b53a576f1f9e9cfa"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
